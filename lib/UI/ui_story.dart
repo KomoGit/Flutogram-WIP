32b25522c75afde4356be14f2a32e161af09aff5
James <entreur@gmail.com>
2022-11-15 19:22:43 +0400

    Small change

--- lib/UI/ui_story.dart
+++ lib/UI/ui_story.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gradient_borders/box_borders/gradient_box_border.dart';
 import 'package:instaui/Data/dat_story.dart';
 
 Widget storyButton(StoryData data, BuildContext context) {
@@ -15,9 +16,18 @@ Widget storyButton(StoryData data, BuildContext context) {
             width: 70,
             height: 70.0,
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(50),
-              border: Border.all(color: Colors.red, width: 2.0),
-            ),
+                borderRadius: BorderRadius.circular(50),
+                border: const GradientBoxBorder(
+                  gradient: LinearGradient(colors: [
+                    Color.fromARGB(254, 218, 117, 255),
+                    Color.fromARGB(250, 126, 30, 255),
+                    Color.fromARGB(214, 41, 118, 255),
+                    Color.fromARGB(150, 47, 191, 255),
+                    Color.fromARGB(79, 91, 213, 255),
+                  ]),
+                  width: 2,
+                ) //Border.all(color: Colors.red, width: 2.0),
+                ),
             child: Padding(
               padding: const EdgeInsets.all(3.0),
               child: Container(
@@ -31,7 +41,11 @@ Widget storyButton(StoryData data, BuildContext context) {
           ),
         ),
         const SizedBox(height: 6.0),
-        Text(data.userName),
+        Text(
+          data.userName,
+          overflow: TextOverflow.ellipsis,
+          textAlign: TextAlign.center,
+        ),
       ],
     ),
   );

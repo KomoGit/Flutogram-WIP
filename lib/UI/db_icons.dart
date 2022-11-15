7ba866d188641165ccbc06b1df61e5ec3ee11a7c
James <entreur@gmail.com>
2022-11-15 17:59:00 +0400

    Custom Instagram UI

--- lib/UI/db_icons.dart
+++ lib/UI/db_icons.dart
@@ -11,4 +11,12 @@ class DBIcons {
       IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
   static const IconData igIco =
       IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData heart = IconData(0xe802, fontFamily: _kFontFam);
+  static const IconData profile = IconData(0xe803, fontFamily: _kFontFam);
+  static const IconData search = IconData(0xe804, fontFamily: _kFontFam);
+  static const IconData home = IconData(0xe805, fontFamily: _kFontFam);
+  static const IconData bookmark = IconData(0xe806, fontFamily: _kFontFam);
+  static const IconData share = IconData(0xe807, fontFamily: _kFontFam);
+  static const IconData comment = IconData(0xe808, fontFamily: _kFontFam);
+  static const IconData add = IconData(0xe809, fontFamily: _kFontFam);
 }

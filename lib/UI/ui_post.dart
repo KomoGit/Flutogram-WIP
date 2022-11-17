ec84b397eb23659965c7cfd30454b15b8c0eb34d
James <entreur@gmail.com>
2022-11-17 14:32:33 +0400

    Post Widget

---
+++ lib/UI/ui_post.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class PostCard extends StatelessWidget {
+  const PostCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black,
+      padding: const EdgeInsets.symmetric(
+        vertical: 10,
+      ),
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(
+              vertical: 4,
+              horizontal: 16,
+            ).copyWith(right: 0),
+            child: Row(
+              children: const [],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

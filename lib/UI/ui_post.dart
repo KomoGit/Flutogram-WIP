501fd7be4e50305d743c58d11bb60336edd2d005
James <entreur@gmail.com>
2022-11-18 11:44:54 +0400

    Better post system.

--- lib/UI/ui_post.dart
+++ lib/UI/ui_post.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:instaui/Data/dat_mock.dart';
+import 'package:instaui/UI/db_icons.dart';
 
 class PostCard extends StatelessWidget {
   const PostCard({super.key});
@@ -6,7 +8,7 @@ class PostCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.black,
+      color: Colors.white,
       padding: const EdgeInsets.symmetric(
         vertical: 10,
       ),
@@ -18,8 +20,90 @@ class PostCard extends StatelessWidget {
               horizontal: 16,
             ).copyWith(right: 0),
             child: Row(
-              children: const [],
+              children: [
+                CircleAvatar(
+                  radius: 16,
+                  backgroundImage: NetworkImage(stories[1].avatarURL),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 8),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          stories[1].userName,
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                IconButton(
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (context) => Dialog(
+                              child: ListView(
+                                  padding: const EdgeInsets.symmetric(
+                                    vertical: 16,
+                                  ),
+                                  shrinkWrap: true,
+                                  children: [
+                                    'Delete',
+                                  ]
+                                      .map(
+                                        (e) => InkWell(
+                                          onTap: () {},
+                                          child: Container(
+                                            padding: const EdgeInsets.symmetric(
+                                                vertical: 12, horizontal: 16),
+                                          ),
+                                        ),
+                                      )
+                                      .toList()),
+                            ));
+                  },
+                  icon: const Icon(Icons.more_vert),
+                ),
+              ],
             ),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.35,
+            width: double.infinity,
+            child: Image.network(
+              stories[1].storyURL,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Row(
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(DBIcons.heart),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(DBIcons.comment),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(DBIcons.share),
+              ),
+              Expanded(
+                child: Align(
+                  alignment: Alignment.bottomRight,
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(DBIcons.bookmark),
+                  ),
+                ),
+              ),
+            ],
           ),
         ],
       ),

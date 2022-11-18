3cdf9867097af7931f9c699765fd947e771aadbd
James <entreur@gmail.com>
2022-11-18 11:44:33 +0400

    Globalization of Mock data.

---
+++ lib/Data/dat_mock.dart
@@ -0,0 +1,39 @@
+import 'package:instaui/Data/dat_story.dart';
+
+List<StoryData> stories = [
+  StoryData(
+    "Your Story",
+    "https://randomuser.me/api/portraits/men/18.jpg",
+    "storyURL",
+  ),
+  StoryData(
+    "Ken Walter",
+    "https://randomuser.me/api/portraits/men/51.jpg",
+    "https://wallpaper.dog/large/20521642.jpg",
+  ),
+  StoryData(
+    "Amanda Simons",
+    "https://randomuser.me/api/portraits/women/73.jpg",
+    "https://wallpaper.dog/large/20521639.jpg",
+  ),
+  StoryData(
+    "Henry Welfam",
+    "https://randomuser.me/api/portraits/men/24.jpg",
+    "https://wallpaper.dog/large/20521643.jpg",
+  ),
+  StoryData(
+    "Troy Cole",
+    "https://randomuser.me/api/portraits/men/6.jpg",
+    "https://wallpaper.dog/large/20521645.jpg",
+  ),
+  StoryData(
+    "Ricardo Martin",
+    "https://randomuser.me/api/portraits/men/26.jpg",
+    "https://wallpaper.dog/large/20521652.jpg",
+  ),
+  StoryData(
+    "Beverly Ward",
+    "https://randomuser.me/api/portraits/women/67.jpg",
+    "https://wallpaper.dog/large/5530554.jpg",
+  ),
+];
